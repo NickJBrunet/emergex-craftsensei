635edb8a27c3eb5bf299cdbb4b5e398f9974703e
Nick <nicholas.brunet@edu.sait.ca>
2026-02-25 15:37:35 -0700

    Setup Database + Installed Missing Libraries

--- backend_server/backend_server/settings.py
+++ backend_server/backend_server/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'backend_server.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "craftsensei",
+        "USER": "postgres",
+        "PASSWORD": "admin",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
@@ -99,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "accounts.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
