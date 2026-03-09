b0b73c68300722b2b6bd24990f33a94fd836baa4
Nathanael Lee <nathanaellee9@Hotmail.com>
2026-03-09 14:26:51 -0600

    updated messenger to use apikey

--- crafty_bot/src/main/java/io/github/craftsensei/api/Messenger.java
+++ crafty_bot/src/main/java/io/github/craftsensei/api/Messenger.java
@@ -12,9 +12,33 @@ import org.bukkit.entity.Player;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
+import org.yaml.snakeyaml.Yaml;
+import java.io.InputStream;
+import java.util.Map;
+
 public class Messenger {
     // called from BasicChatMessageHandler
     public CompletableFuture<String> sendToDjango(Player player, String message) {
+        Yaml yaml = new Yaml();
+        String baseUrl = "";
+        String apiKey = "";
+
+        // Load the YAML file from the resources folder
+        try (InputStream inputStream = Messenger.class.getClassLoader().getResourceAsStream("config.yml")) {
+            if (inputStream == null) {
+                System.err.println("YAML file not found in resources folder!");
+            }
+            // Parse the YAML file into a Map
+            Map<String, Object> data = yaml.load(inputStream);
+
+            // Access values by keys
+            baseUrl = (String) data.get("craft-api-base-url");
+            apiKey = (String) data.get("craft-api-key");
+
+        } catch (Exception e) {
+           return CompletableFuture.failedFuture(e);
+        }
+
         try {
             // declare variables
             HttpClient client = HttpClient.newHttpClient();
@@ -29,8 +53,9 @@ public class Messenger {
 
             // create request
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("http://localhost:8000/minecraft/chat/"))
+                    .uri(URI.create(baseUrl))
                     .header("Content-Type", "application/json")
+                    .header("Authorization", "Bearer " + apiKey)
                     .POST(HttpRequest.BodyPublishers.ofString(json))
                     .build();
 
@@ -47,3 +72,5 @@ public class Messenger {
         }
     }
 }
+
+
