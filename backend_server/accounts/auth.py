0cefac995ddf961b149a183d61e02c0b411bf9a2
Nick <nicholas.brunet@edu.sait.ca>
2026-04-11 22:41:34 -0600

    Final Auth Issue Fixes

--- backend_server/accounts/auth.py
+++ backend_server/accounts/auth.py
@@ -8,15 +8,11 @@ User = get_user_model()
 class JWTAuth:
     def __call__(self, request):
         key = request.COOKIES.get("access_token")
-
-        print("RAW TOKEN:", repr(key))  # debug
-
         if not key:
             return None
 
         try:
             validated = AccessToken(key)
-            return User.objects.get(id=validated["user_id"])
+            return User.objects.get(id=int(validated["user_id"]))
         except Exception as e:
-            print("JWT ERROR:", e)
             return None
