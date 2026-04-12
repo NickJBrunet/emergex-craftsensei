138ecea2e677b98e0d8991d829205bd62c46a36f
Nick <nicholas.brunet@edu.sait.ca>
2026-04-11 22:50:08 -0600

    Final Auth Issue Fixes 3.0

--- backend_server/accounts/auth.py
+++ backend_server/accounts/auth.py
@@ -5,14 +5,18 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class JWTAuth:
+class JWTAuth(APIKeyCookie):
+    param_name = "access_token"
+
     def authenticate(self, request, key):
         try:
             validated = AccessToken(key)
-            user = User.objects.get(id=int(validated["user_id"]))
 
-            request.user = user  # ✅ IMPORTANT
-            return user
+            user_id = validated.get("user_id")
+            if not user_id:
+                return None
+
+            return User.objects.get(pk=user_id)
 
         except Exception as e:
             print("JWT ERROR:", e)
