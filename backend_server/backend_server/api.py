720da49a39c9cae8ffe56a712da1e56e15573102
Nick <nicholas.brunet@edu.sait.ca>
2026-04-14 20:36:17 -0600

    More CSRF Errors Creeping In

--- backend_server/backend_server/api.py
+++ backend_server/backend_server/api.py
@@ -2,7 +2,7 @@ from ninja import NinjaAPI
 from accounts.api import router as accounts_router
 from servers.api import router as servers_router
 
-api = NinjaAPI(csrf=False)  # type: ignore
+api = NinjaAPI()
 
 api.add_router("/auth/", accounts_router)
 api.add_router("/servers/", servers_router)
